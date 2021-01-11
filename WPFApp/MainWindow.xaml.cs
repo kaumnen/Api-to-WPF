6a0ab0a49db181a3ba717ce6a3e018e75ffdfc04
Aleksej Komnenovic <main@komnen.xyz>
2021-01-11 18:44:45 +0100

    Initialize project
    Initializes project with api helper class and setting up http client.

---
+++ WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using DemoLibrary;
+
+namespace WPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            APIHelper.InitializeClient();
+        }
+    }
+}
